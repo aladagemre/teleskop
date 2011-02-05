2d73f0b74491c2452eae094e06294e43b74d439a
Ahmet Emre Aladağ <aladagemre@gmail.com>
2011-02-05 14:25:37 +0200

    Setup file, readme file modification

--- src/setup.py
+++ src/setup.py
@@ -1,25 +1,25 @@
 __author__="emre"
-__date__ ="$Jul 4, 2010 7:38:17 PM$"
+__date__ ="$05.Feb.2011 14:16:14$"
 
 from setuptools import setup,find_packages
 
 setup (
   name = 'teleskop',
   version = '0.1',
-  packages = find_packages(),
+  packages = ('teleskop',),
 
   # Declare your packages' dependencies here, for eg:
-  install_requires=['foo>=3'],
+  install_requires=['PIL>=1'],
 
   # Fill in these to make your Egg ready for upload to
   # PyPI
   author = 'emre',
   author_email = '',
 
-  summary = 'Just another Python package for the cheese shop',
-  url = '',
-  license = '',
-  long_description= 'Long description of the package',
+  summary = 'Queued image resizer',
+  url = 'https://github.com/aladagemre/teleskop',
+  license = 'GPL v3',
+  long_description= 'Queued image resizer',
 
   # could also include long_description, download_url, classifiers, etc.
 
